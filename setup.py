0354b3f10c8338b0d90e70b8f8e1449bfc5a5e57
Šarūnas Navickas <sarunas.navickas@adform.com>
2015-08-16 20:08:44 +0300

    Initial struct

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+from pip.req import parse_requirements
+
+
+install_reqs = list(parse_requirements("requirements", session = {}))
+def version():
+    return __import__("wutu").get_version()
+
+setup(  name="wutu",
+        version=version(),
+        description="A minimalistic python-angular framework",
+        author="Šarūnas Navickas",
+        author_email="zaibacu@gmail.com",
+        license="MIT",
+        packages=["wutu"],
+        install_requires=[str(ir.req) for ir in install_reqs]
+    )
---
+++ wutu/version.py
@@ -0,0 +1,4 @@
+VERSION = (0, 0, 1)
+
+def get_version(version=VERSION):
+	return ".".join(map(lambda x: str(x), version))
