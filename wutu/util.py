70a4afb4fb6c8c606f46b55ac498c4f6ba06a902
Šarūnas Navickas <sarunas.navickas@adform.com>
2015-08-17 09:10:48 +0300

    Load modules by config

--- wutu/util.py
+++ wutu/util.py
@@ -1,8 +1,15 @@
 import os
+import configparser
 
+
+def is_module_enabled(module):
+	conf = configparser.ConfigParser()
+	conf.read(current("modules", module, "module.ini"))
+	return conf.getboolean("DEFAULT", "enabled")
 
 def get_modules():
-	return os.listdir(current("modules"))
+	modules = os.listdir(current("modules"))
+	return list(filter(lambda mod: is_module_enabled(mod),  modules))
 
-def current(dir):
-	return os.path.join(os.getcwd(), dir)
+def current(*dir):
+	return os.path.join(os.getcwd(), *dir)
