6eb630653e3596b125aa0c3b1501e9bda0f6dc8a
Šarūnas Navickas <sarunas.navickas@adform.com>
2015-08-17 08:48:21 +0300

    No need for printing here

--- wutu/tests/util_tests.py
+++ wutu/tests/util_tests.py
@@ -4,5 +4,4 @@ from wutu.util import *
 class UtilTests(unittest.TestCase):
 	def test_directory_scan(self):
 		modules = get_modules()
-		print(modules)
 		self.assertEqual(modules, ["test_module"])
