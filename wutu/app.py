b2173edf347f4c6bf31820a7a1e85e8cbf3c6277
Šarūnas Navickas <sarunas.navickas@adform.com>
2015-08-18 20:45:06 +0300

    Restructuring

--- wutu/app.py
+++ wutu/app.py
@@ -1,5 +1,6 @@
 import sys
-from flask import Flask
+import jinja2
+from flask import Flask, render_template
 from flask_restful import Api
 
 from wutu.util import *
@@ -7,9 +8,13 @@ from wutu.util import *
 app = Flask(__name__)
 api = Api(app)
 
+def main(index, locator, *args, **kwargs):
+    app.jinja_loader = jinja2.FileSystemLoader(locator())
+    modules = [ load_module(module, api=api) for module in get_modules() ]
+    @app.route("/")
+    def index_page():
+        return render_template(index, modules=modules)
 
-def main(*args, **kwargs):
-    [ load_module(module, api=api) for module in get_modules() ]
     app.run(*args, **kwargs)
 
 if __name__ == "__main__":
--- wutu/functional_tests/site_tests.py
+++ wutu/functional_tests/site_tests.py
@@ -3,12 +3,13 @@ import unittest
 from multiprocessing import Process
 
 from wutu import app
+from wutu.test_util import *
 
 def start_server():
-    app.main(host="localhost", port=5555, debug=True)
+    app.main(index="test.html", locator=test_locator(), host="localhost", port=5555, debug=True)
 
 
-class Functional(unittest.TestCase):
+class SiteTests(unittest.TestCase):
     def setUp(self):
         self.browser = webdriver.Firefox()
 
@@ -17,6 +18,7 @@ class Functional(unittest.TestCase):
 
     def test_module_get(self):
         self.browser.get("http://localhost:5555/")
+        self.assertEqual(self.browser.title, "test")
 
 if __name__ == "__main__":
     p = Process(target=start_server)
