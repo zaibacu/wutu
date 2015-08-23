c5ca2adebd05dcbea52c95c79ebddd835f3946d9
Šarūnas Navickas <sarunas.navickas@adform.com>
2015-08-23 18:43:02 +0300

    First (working) service test

--- wutu/tests/specs/basic_crud_spec.js
+++ wutu/tests/specs/basic_crud_spec.js
@@ -1,23 +1,24 @@
 'use strict';
 
 describe("CRUD service", function(){
+	var service, $httpBackend;
 
 	beforeEach(module("wutu"));
-	beforeEach(function(){
-		base_url = function(){ return "http://localhost:5555/"; };
-	});
-	var service;
+
+	beforeEach(inject(function($injector){
+		$httpBackend = $injector.get("$httpBackend");
+	}));
+
 	beforeEach(inject(function(_TestModuleService_){
 		service = _TestModuleService_;
 	}));
 
-	afterEach(inject(function($rootScope) {
-	  $rootScope.$apply();
-	}));
-
 	it("handle create operation", function(done){
 		service.get(1).then(function(response){
-			expect(response).toBe(1);
+			expect(response.data.result).toBe(1);
 		}).finally(done);
+
+		$httpBackend.expectGET("/test_module/1").respond({result: 1})
+		$httpBackend.flush();
 	});
 });
