5ea1a97f1a57da73a92f9d456de3196d58027a0f
Šarūnas Navickas <sarunas.navickas@adform.com>
2015-08-23 18:50:56 +0300

    More async tests

--- wutu/tests/specs/basic_crud_spec.js
+++ wutu/tests/specs/basic_crud_spec.js
@@ -14,11 +14,38 @@ describe("CRUD service", function(){
 	}));
 
 	it("handle create operation", function(done){
-		service.get(1).then(function(response){
-			expect(response.data.result).toBe(1);
+		service.post(1, {"test": "#1"}).then(function(response){
+			expect(response.data.id).toBe(1);
 		}).finally(done);
 
-		$httpBackend.expectGET("/test_module/1").respond({result: 1})
+		$httpBackend.expectPOST("/test_module/1", {"test": "#1"}).respond({id: 1})
+		$httpBackend.flush();
+	});
+
+	it("handle read operation", function(done){
+		service.get(1).then(function(response){
+			expect(response.data).toEqual({"test": "#1"});
+		}).finally(done);
+
+		$httpBackend.expectGET("/test_module/1").respond({"test": "#1"})
+		$httpBackend.flush();
+	});
+
+	it("handle update operation", function(done){
+		service.put(1, {"test": "#2"}).then(function(response){
+			expect(response.data).toEqual({"test": "#2"});
+		}).finally(done);
+
+		$httpBackend.expectPUT("/test_module/1", {"test": "#2"}).respond({"test": "#2"})
+		$httpBackend.flush();
+	});
+
+	it("handle delete operation", function(done){
+		service.delete(1).then(function(response){
+			expect(response.data).toBe(true);
+		}).finally(done);
+
+		$httpBackend.expectDELETE("/test_module/1").respond(true)
 		$httpBackend.flush();
 	});
 });
