e2eb309688391fd7809ef2f3a79fb7b4b2154d69
Šarūnas Navickas <sarunas.navickas@adform.com>
2015-08-25 08:22:42 +0300

    Introduce protractor

---
+++ wutu/tests/e2e.conf.js
@@ -0,0 +1,4 @@
+exports.config = {
+  seleniumAddress: "http://localhost:4444/wd/hub",
+  specs: ["specs/*_e2e_spec.js"]
+};
--- wutu/tests/js_tests.py
+++ wutu/tests/js_tests.py
@@ -6,6 +6,7 @@ from test_util import *
 class JsTests(unittest.TestCase):
     def setUp(self):
         execute("npm install")
+        execute("./node/modules/protractor/bin/webdriver-manager update")
         self.p = Process(target=start_server)
         self.p.start()
 
@@ -15,3 +16,6 @@ class JsTests(unittest.TestCase):
     def test_run_unit_tests(self):
         result = execute("./node_modules/karma/bin/karma start")
         self.assertTrue(result)
+
+    def test_run_e2e_tests(self):
+        result = execute("./node_modules/protractor/bin/protractor e2e.conf.js")
