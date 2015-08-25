2c0dea5e0079022f0f826a46e237220292120e31
Šarūnas Navickas <sarunas.navickas@adform.com>
2015-08-25 08:15:38 +0300

    Working JS tests

--- wutu/tests/js_tests.py
+++ wutu/tests/js_tests.py
@@ -1,16 +1,17 @@
 import unittest
-from Naked.toolshed.shell import execute_js
+from Naked.toolshed.shell import execute_js, execute
+from multiprocessing import Process
 from test_util import *
-from wutu.util import load_module
-from wutu.compiler import create_base
 
 class JsTests(unittest.TestCase):
     def setUp(self):
-        mod = load_module("test_module", test_locator)
-        stream = create_stream()
-        with open("tmp/test_module_service.js", "w") as f:
-            f.write(create_base(stream))
-            f.write(mod.get_service(stream))
+        execute("npm install")
+        self.p = Process(target=start_server)
+        self.p.start()
 
-    def test_validate_syntax(self):
-        self.assertTrue(execute_js("tmp/test_module_service.js"))
+    def tearDown(self):
+        self.p.terminate()
+
+    def test_run_unit_tests(self):
+        result = execute("./node_modules/karma/bin/karma start")
+        self.assertTrue(result)
