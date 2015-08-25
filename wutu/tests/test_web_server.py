e634b7b8bcaaa6f03e8a5f52a9fa23ef9ca4efd2
Šarūnas Navickas <sarunas.navickas@adform.com>
2015-08-25 07:45:46 +0300

    Server for running tests

---
+++ wutu/tests/test_web_server.py
@@ -0,0 +1,9 @@
+from wutu import app
+from test_util import *
+
+def start_server():
+    testing_app = app.create(index="test.html", locator=test_locator)
+    testing_app.run(host="localhost", port=5555, debug=True, use_reloader=False)
+
+if __name__ == "__main__":
+    start_server()
