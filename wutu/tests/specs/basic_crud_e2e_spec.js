bcfd53f78035baf2f8fbe8bc57124be755ca86c1
Šarūnas Navickas <sarunas.navickas@adform.com>
2015-08-26 08:13:54 +0300

    Some basic functionality

--- wutu/tests/specs/basic_crud_e2e_spec.js
+++ wutu/tests/specs/basic_crud_e2e_spec.js
@@ -1,2 +1,22 @@
 describe("Basic CRUD operation from front-end", function(){
+	beforeEach(function(){
+		browser.get("http://localhost:5555");
+	});
+
+	it("should let you to add note", function(){
+		var note = element(by.model("note"));
+		note.sendKeys("tempnote");
+		note.submit();
+
+		var note = element(by.xpath("//span[text() = 'tempnote']"));
+		expect(note.isPresent()).toBe(true);
+	});
+
+	it("should let you delete selected note", function(){
+		var delButton = element(by.xpath("//span[text() = 'tempnote']")).element(by.xpath("../button"));
+		delButton.click();
+
+		var note = element(by.xpath("//span[text() = 'tempnote']"));
+		expect(note.isPresent()).toBe(false);
+	});
 });
