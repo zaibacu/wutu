0e0ba0c3e44701709125e62fc416049e17dd25c3
Šarūnas Navickas <sarunas.navickas@adform.com>
2015-08-26 07:17:06 +0300

    phantomJS does not work with angular...

--- wutu/tests/e2e.conf.js
+++ wutu/tests/e2e.conf.js
@@ -1,7 +1,8 @@
 exports.config = {
   seleniumAddress: "http://localhost:4444/wd/hub",
   specs: ["specs/*_e2e_spec.js"],
+  framework: "jasmine2",
   capabilities: {
-    browserName: "phantomjs"
+    browserName: "firefox"
   }
 };
