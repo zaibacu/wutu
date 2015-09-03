58c30421e488c89f5317b2845e8dca8381124c66
Šarūnas Navickas <sarunas.navickas@adform.com>
2015-09-03 18:03:27 +0300

    Doc strings for module

--- wutu/module.py
+++ wutu/module.py
@@ -5,30 +5,64 @@ import inspect
 
 
 def module_locator(module, *directory):
-    get_module_dir = lambda mod: os.path.dirname(inspect.getmodule(mod.__class__).__file__)
-    return os.path.join(get_module_dir(module), *directory)
+	"""
+	Custom locator for modules
+	:param module: module itself
+	:param directory: search directory
+	:return:
+	"""
+	get_module_dir = lambda mod: os.path.dirname(inspect.getmodule(mod.__class__).__file__)
+	return os.path.join(get_module_dir(module), *directory)
 
 
 class Module(Resource):
-    def __init__(self):
-        super(Module, self).__init__()
+	"""
+	Base class for all modules
+	"""
+	def __init__(self):
+		super(Module, self).__init__()
 
-    def mediatypes(self):
-        return ["application/json", "text/javascript"]
+	def mediatypes(self):
+		"""
+		Mediatypes override
+		:return:
+		"""
+		return ["application/json", "text/javascript"]
 
-    def create_service(self, stream):
-        create_service_js(stream, self)
+	def create_service(self, stream):
+		"""
+		Constructs AngularJS service
+		:param stream:
+		:return:
+		"""
+		create_service_js(stream, self)
 
-    def get_service(self):
-        stream = create_stream()
-        self.create_service(stream)
-        return get_data(stream)
+	def get_service(self):
+		"""
+		Constructs AngularJS service without stream
+		:return:
+		"""
+		stream = create_stream()
+		self.create_service(stream)
+		return get_data(stream)
 
-    def get_controller(self):
-        return ""
+	def get_controller(self):
+		"""
+		Returns AngularJS controller
+		:return:
+		"""
+		return ""
 
-    def get_identifier(self):
-        return ["id"]
+	def get_identifier(self):
+		"""
+		Returns parameters for querying
+		:return:
+		"""
+		return ["id"]
 
-    def get_name(self):
-        return self.__name__
+	def get_name(self):
+		"""
+		Returns module name
+		:return:
+		"""
+		return self.__name__
