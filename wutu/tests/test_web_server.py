4806188a458511934659ea70dcda177a7e82c720
Šarūnas Navickas <sarunas.navickas@adform.com>
2015-09-13 10:55:15 +0300

    Use new API

--- wutu/tests/test_web_server.py
+++ wutu/tests/test_web_server.py
@@ -1,20 +1,21 @@
-from test_util import *
-from util import *
+from wutu import Wutu
+from wutu.test_util import prepare_db
+
 
 def start_server():
 	prepare_db()
 
-	testing_app = app.create(index="test.html", locator=test_locator)
+	app = Wutu(index="test.html")
 
-	@create_module(testing_app.api)
+	@app.create_module
 	def test_module_2():
 		return {"get": lambda req: "Hello"}
 
-	@create_module(testing_app.api)
+	@app.create_module
 	def test_module_3():
 		return {"get": lambda req, id: "Hello again, {0}".format(id)}
 
-	testing_app.run(host="localhost", port=5555, debug=True, use_reloader=False)
+	app.run(host="localhost", port=5555, debug=True, use_reloader=False)
 
 if __name__ == "__main__":
 	start_server()
