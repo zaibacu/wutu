b967060cc4e4c7a6a1730114070f757c9b2c13d2
Šarūnas Navickas <sarunas.navickas@adform.com>
2015-09-24 18:56:32 +0300

    Updated bench

--- wutu/bench/compiler_bench.py
+++ wutu/bench/compiler_bench.py
@@ -1,17 +1,20 @@
-from wutu.compiler.common import create_service_js, create_stream
+from wutu.compiler.common import create_stream
+from wutu.compiler.service import create_service_js
+from wutu.compiler.controller import create_controller_js
 from wutu.util import *
 
 
 def handle_creation(module, stream):
-	create_service_js(stream, module)
+    create_service_js(stream, module)
+    create_controller_js(stream, module)
 
 
 def main():
-	mod = Module()
-	mod.__name__ = "test_module"
-	stream = create_stream()
-	handle_creation(mod, stream)
+    mod = Module()
+    mod.__name__ = "test_module"
+    stream = create_stream()
+    handle_creation(mod, stream)
 
 if __name__ == "__main__":
-	import cProfile
-	cProfile.run("main()")
+    import cProfile
+    cProfile.run("main()")
