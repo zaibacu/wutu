fe003ad5f4420b1fd52ccb32f42e82893e5beb7f
Šarūnas Navickas <sarunas.navickas@adform.com>
2015-10-07 07:20:46 +0300

    Fix for controller and service behavior

--- wutu/compiler/controller.py
+++ wutu/compiler/controller.py
@@ -6,19 +6,22 @@ def create_controller_js(stream, module):
     :return:
     """
     from wutu.util import get_implemented_methods
-    from wutu.compiler.grammar import Provider, Function, unwraps
+    from wutu.compiler.grammar import Provider, Function, SimpleDeclare
     stream.write("wutu.controller('{0}Controller', ".format(module.__class__.__name__))
     service = Provider("{0}Service".format(module.__class__.__name__))
     scope = Provider("$scope")
     methods = get_implemented_methods(module)
+    entity_name = "{0}_list".format(module.get_entity_name())
     params = module.get_identifier()
     post_params = params + ("data",)
     put_params = ("data",)
-    scope["{0}_list".format(module.get_entity_name())] = service.list()
+
+    scope[entity_name] = service.list()
+    scope.refresh = Function(None, body=[SimpleDeclare(scope[entity_name].compile(), service.list())])
     scope["get_{0}".format(module.get_entity_name())] = Function(params, returns=service.get(*params))
-    scope["create_{0}".format(module.get_entity_name())] = Function(put_params, returns=service.put(*put_params))
-    scope["update_{0}".format(module.get_entity_name())] = Function(post_params, body=[service.post(*post_params)])
-    scope["remove_{0}".format(module.get_entity_name())] = Function(params, body=[service.delete(*params)])
+    scope["create_{0}".format(module.get_entity_name())] = Function(put_params, body=[service.put(*put_params), scope.refresh()])
+    scope["update_{0}".format(module.get_entity_name())] = Function(post_params, body=[service.post(*post_params), scope.refresh()])
+    scope["remove_{0}".format(module.get_entity_name())] = Function(params, body=[service.delete(*params), scope.refresh()])
     impl = Function([scope.name, service.name], body=scope.assignments)
     stream.write(impl.compile())
     stream.write(");")
--- wutu/compiler/service.py
+++ wutu/compiler/service.py
@@ -14,8 +14,8 @@ def create_service_js(stream, module):
     extended = "+ \"/\" + {0} + \"/\"".format(" + \"/\" + ".join(params)) if len(params) > 0 else ""
     full_url = "base_url() + url {0}".format(extended)
     obj.add_member("list", Function(None, *unwraps(http.get("base_url() + url"))))
-    obj.add_member("get", Function(params, *unwraps(http.get(full_url))))
-    obj.add_member("put", Function(("data",), *unwraps(http.put("base_url() + url", "data"))))
+    obj.add_member("get", Function(params + ("result",), *unwraps(http.get(full_url), "result")))
+    obj.add_member("put", Function(("data", "result",), *unwraps(http.put("base_url() + url", "data"), "result")))
     obj.add_member("post", Function(params=params + ("data",), returns=http.post(full_url, "data")))
     obj.add_member("delete", Function(params=params, returns=http.delete(full_url)))
     impl = Function([http.name],
