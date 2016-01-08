5ee572144403bbfb7771c314370e24db003b5e4d
Šarūnas Navickas <sarunas.navickas@adform.com>
2016-01-08 12:46:06 +0200

    Basic module wrapping

--- wutu/compiler/module.py
+++ wutu/compiler/module.py
@@ -1,2 +1,13 @@
-def create_module_js(stream, mod):
-    pass
+from wutu.module import Module
+
+
+def create_module_js(stream, mod: Module):
+    """
+    Creates AngularJS module for our module
+    :param stream: api stream - to put data
+    :param mod: our module
+    :return:
+    """
+    from wutu.compiler.snippet import compile_snippet
+    module = compile_snippet("angular_module.html", name=mod.get_name())
+    stream.write(str(compile_snippet("require_wrapper.html", content=module)))
