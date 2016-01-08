fa799c15b3ed8ee156f7f6cdf33104b6244237eb
Šarūnas Navickas <sarunas.navickas@adform.com>
2016-01-08 19:02:32 +0200

    Fix depenency issues

--- wutu/compiler/module.py
+++ wutu/compiler/module.py
@@ -9,7 +9,10 @@ def create_module_js(stream, mod, depends=None):
     from wutu.compiler.snippet import compile_snippet
     from wutu.compiler.common import create_stream, get_data
     inner_stream = create_stream()
-    inner_stream.write(str(compile_snippet("angular_module.html", name=mod.get_name(), declare="mod", dep=depends)))
+    inner_stream.write(str(compile_snippet("angular_module.html",
+                                           name=mod.get_name(),
+                                           declare="mod",
+                                           dep=depends if depends else [])))
     yield inner_stream
     yield stream.write(str(compile_snippet("require_wrapper.html", content=get_data(inner_stream))))
 
